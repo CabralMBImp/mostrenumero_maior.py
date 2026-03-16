efc10ab25c6ffbba85c8a53fa1570f7151983d79
CabralMBImp <260758418+CabralMBImp@users.noreply.github.com>
2026-03-15 21:51:20 -0300

    Delete Ex2.py

--- Ex2.py
+++
@@ -1,9 +0,0 @@
-# Pedimos o valor ao usuário e convertemos para número inteiro (int)
-valor = int(input("Digite um número inteiro: "))
-
-# Verificamos se é maior ou igual a zero
-if valor >= 0:
-    print("O valor é POSITIVO.")
-else:
-    # Se a condição acima for falsa (ou seja, menor que zero), cai aqui
-    print("O valor é NEGATIVO.")
